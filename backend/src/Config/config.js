c0f48ec78fd14f71e49d0a3f395dad220dc0c75a
AnkitParte <ankitparte80@gmail.com>
2022-09-30 11:53:42 +0530

    dotenv

--- backend/src/Config/config.js
+++ backend/src/Config/config.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 //let localurl ="mongodb://127.0.0.1:27017/hellobonsai"
-function connect() {
-    let url = `mongodb+srv://vijay:12345@cluster0.ggexotb.mongodb.net/hellobonsai`;
+function connect(url) {
     return mongoose.connect(url);
 }
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,13 @@ const userRouter = require("./Features/Users/userRouter");
 const clientRouter = require("./Features/Clients/clientRouter");
 const projectRouter = require("./Features/Projects/projectsRouter");
 const taskRouter = require("./Features/Tasks/tasksRouter");
+const dotenv = require("dotenv");
+dotenv.config();
 const connect = require("./Config/config");
+
+const PORT = process.env.PORT || 6000;
+const API = process.env.MONGO_URL || "";;
+
 
 const app = express();
 app.use(cors());
@@ -13,7 +19,7 @@ app.use("/users", userRouter);
 app.use("/clients", clientRouter);
 app.use("/projects", projectRouter);
 app.use("/tasks", taskRouter);
-app.listen(5000, async () => {
-    await connect();
+app.listen(PORT, async () => {
+    await connect(API);
     console.log("User heat the server!!");
 })
